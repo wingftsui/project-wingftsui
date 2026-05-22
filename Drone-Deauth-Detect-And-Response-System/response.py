2e09ea1c9a4f82bfd68162de5126065a0f78daea
wingftsui <wing.f.tsui@students.plymouth.ac.uk>
2026-05-22 22:47:35 +0800

    commit

--- Drone-Deauth-Detect-And-Response-System/response.py
+++ Drone-Deauth-Detect-And-Response-System/response.py
@@ -6,5 +6,8 @@ def load_drone_json(filepath='drones.json'):
     try:
         with open(filepath,'r') as file:
             return json.load(file)
-    except FileNotFoundError or json.JSONDecodeError:
-        print(f'Please provide the correct drone json file!')
+    except FileNotFoundError:
+        print(f'No json file found!')
+    except json.JSONDecodeError:
+        print(f'Wrong file format. It should be json file')
+
