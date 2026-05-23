d141379f1e5e9b93ec235ba97a355477b1718d15
wingftsui <wing.f.tsui@students.plymouth.ac.uk>
2026-05-23 15:15:16 +0800

    Response module programming

--- Drone-Deauth-Detect-And-Response-System/response.py
+++ Drone-Deauth-Detect-And-Response-System/response.py
@@ -7,8 +7,52 @@ def load_drone_json(filepath='drones.json'):
         with open(filepath,'r') as file:
             return json.load(file)
     except FileNotFoundError:
-        print(f'No json file found!')
+        print(f'No json setting file found!')
     except json.JSONDecodeError:
         print(f'Wrong file format. It should be json file')
 
-def trigger_land(drone_model=None,):
+def trigger_land(drone_model=None,custom_ip=None):
+
+    # Check if the json setting file is here. 
+    # It records the Drone's profile data.
+    # The details of the json content are in the readme file under "Drone-Deauth-Detect-And-Response-System" folder.
+    if drone_model is None:
+        print("Please provide drone_model in the json setting file.")
+        return
+    
+    profiles=load_drone_json()    
+    
+    # drone_model is absent in the json setting file.
+    if drone_model not in profiles:
+        print(f'Cannot find dron_model in json setting file.')
+        return
+    
+    profile=profiles[drone_model]
+    target_ip=custom_ip if custom_ip else profile["default_ip"]
+    target_port=profile['port']
+
+    print(f"\n Will send defense land command ")
+
+    sock=socket.socket(socket.AF_INET, socket.SOCK_)
+    try:
+        if profile.get("initial_cmd"):
+            print("send intial command")
+            # Change string into UTF-8
+            initial_bytes=profile["initial_command"].encode('utf-8')
+            sock.sendto(initial_bytes,(target_ip,target_port))
+            time.sleep(0.5)
+
+        if profile.get("land_cmd"):
+            print("Send land command now")
+            land_bytes=profile["land_command"].encode('utf-8')
+            sock.sendto(land_bytes, (target_ip,target_port))
+            print("Land command sent successfully")
+        
+        else:
+            print("Json setting file has no land commond")
+    except Exception as e:
+        print(f"Unsuccess : {e}")
+    
+    finally:
+        sock.close()
+        print("Close.\n")
